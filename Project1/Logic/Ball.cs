611b12060d5449a91f377a0f915696ed68236177
Bednarooo <236502@edu.p.lodz.pl>
2022-04-21 18:41:22 +0200

    class Ball added

--- Project1/Logic/Ball.cs
+++ Project1/Logic/Ball.cs
@@ -4,8 +4,25 @@ using System.Text;
 
 namespace Logic
 {
-    public class Ball
+    internal class Ball
     {
+        public int xPosition { get; set; }
+        public int yPosition { get; set; }
 
+        private int xSpeed { get; set; }
+        private int ySpeed { get; set; }
+
+        public int radius { get; set; }
+        public string color { get; }
+
+        public Ball(int xPosition, int yPosition, int xSpeed, int ySpeed, int radius)
+        {
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
+            this.xSpeed = xSpeed;
+            this.ySpeed = ySpeed;
+            this.radius = radius;
+            this.color = "#fc1100";
+        }
     }
 }
