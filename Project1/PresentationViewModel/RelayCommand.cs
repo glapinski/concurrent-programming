f0ea398d5c86dd56e6d237988d9e5820372c6b21
glapinski <236534@edu.p.lodz.pl>
2022-04-28 16:28:26 +0200

    ViewModel relaycommand

---
+++ Project1/PresentationViewModel/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Presentation.ViewModel
+{
+    internal class RelayCommand : ICommand
+    {
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Func<object,bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void Execute(object obj)
+        {
+            _execute(obj);
+        }
+
+        public bool CanExecute(object obj)
+        {
+            if (_canExecute != null)
+            {
+                return _canExecute(obj);
+            }
+            return false;
+        }
+    }
+}
--- Project1/PresentationViewModel/ViewModel.cs
+++ Project1/PresentationViewModel/ViewModel.cs
@@ -10,6 +10,16 @@ namespace Presentation.ViewModel
 {
     public class ViewModel : BindableBase
     {
+
+        public ViewModel()
+        {
+            StartButtonClicked = new RelayCommand(Execute, CanExecute);
+
+            uint windowWidth = 300;
+            uint windowHeight = 300;
+
+            _modelAbstractApi = ModelAbstractApi.CreateApi(windowWidth, windowHeight);
+        }
         private ModelAbstractApi _modelAbstractApi;
 
         public ICommand StartButtonClicked { get; set; }
@@ -19,5 +29,45 @@ namespace Presentation.ViewModel
             get => _modelAbstractApi.GetBalls();
 
         }
+
+        public uint WindowWidth
+        {
+            get => _modelAbstractApi.WindowWidth;
+            set => _modelAbstractApi.WindowWidth = value;
+        }
+
+        public uint WindowHeight
+        {
+            get => _modelAbstractApi.WindowHeight;
+            set => _modelAbstractApi.WindowHeight = value;
+        }
+
+        public bool StartClicked
+        {
+            get { return _modelAbstractApi.BeginSimulationClicked; }
+            set { _modelAbstractApi.BeginSimulationClicked = value; }
+        }
+
+        public string BallsNumber
+        {
+            get { return _modelAbstractApi.BallsNumber; }
+            set { _modelAbstractApi.BallsNumber = value; }
+        }
+
+        private void Execute(object value)
+        {
+            uint ballsNumber;
+            if(uint.TryParse(BallsNumber, out ballsNumber))
+            {
+                StartClicked = true;
+                _modelAbstractApi.CreateBalls(ballsNumber);
+            }
+        }
+
+        private bool CanExecute(object value)
+        {
+            return !StartClicked;
+        }
+
     }
 }
