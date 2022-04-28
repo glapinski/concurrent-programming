a3ba43f1e75c3aff54d959fe95bb7c5c524b9eb4
glapinski <236534@edu.p.lodz.pl>
2022-04-28 18:15:20 +0200

    ball class changes

--- Project1/Logic/Ball.cs
+++ Project1/Logic/Ball.cs
@@ -1,89 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
 
 namespace Logic
 {
-    public abstract class BallAbstract
+    public class Ball 
     {
-        public abstract float xPosition { get; set; }
-        public abstract float yPosition { get; set; }
-        public abstract float xSpeed { get; set; }
-        public abstract float ySpeed { get; set; }
-        public abstract float radius { get; set; }
+        private float x { get; set; }
+        private float y { get; set; }
+        private float xS { get; set; }
+        private float yS { get; set; }
+        private float r { get; set; }
 
-        public abstract void UpdatePosition();
-        public abstract void ChangeDirection(char axis);
-        public static BallAbstract CreateAPI(float x, float y, float xSpeed, float ySpeed, float radius)
+        public Ball()
         {
-            return new Ball(x, y, xSpeed, ySpeed, radius);
-        }
-    }
+            Random rng = new Random();
+            x = rng.Next(1, 500);
+            y = rng.Next(1, 500);
 
-    internal class Ball : BallAbstract
-    {
-        private float x;
-        private float y;
-        private float xS;
-        private float yS;
-        private float r;
-        public override float xPosition
-        {
-            get => x;
-            set => x = value;
-        }
-        public override float yPosition 
-        { 
-            get => y;
-            set => y = value; 
-        }
-        public override float xSpeed 
-        { 
-            get => xS;
-            set => xS = value; 
-        }
-        public override float ySpeed 
-        { 
-            get => yS; 
-            set => yS = value; 
-        }
-        public override float radius 
-        { 
-            get => r; 
-            set => r = value; 
+            xS = rng.Next(1, 5);
+            yS = rng.Next(1, 5);
         }
 
-        public Ball(float xPosition, float yPosition, float xSpeed, float ySpeed, float radius)
+        public void updatePosition(int axis)
         {
-            this.xPosition = xPosition;
-            this.yPosition = yPosition;
-            this.xSpeed = xSpeed;
-            this.ySpeed = ySpeed;
-            this.radius = radius;
-        }
+            float x2 = x + xS;
+            float y2 = y + yS;
 
-        public override void UpdatePosition()
-        {
-            this.xPosition += this.xSpeed;
-            this.yPosition += this.ySpeed;
-        }
-
-        public override void ChangeDirection(char axis)
-        {
-            if (axis == 'x')
+            if (x2 > axis || x < 0)
             {
-                if (this.xSpeed > 0)
-                {
-                    this.xSpeed *= -1;
-                }
+                xS = -xS;
             }
-            else if (axis == 'y')
+            if(y2 > axis || y < 0)
             {
-                if (this.ySpeed > 0)
-                {
-                    ySpeed *= -1;
-                }
+                yS = -yS;
             }
         }
     }
---
+++ Project1/Logic/Region.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Region
+    {
+        public int size { get; set; }
+
+        public List<Ball> _balls { get; set; }
+        private Task changePosition;
+    }
+}
